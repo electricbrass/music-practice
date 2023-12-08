2607c0d979dd2baf2175fd24114542c526f21ef0
Mia <miadawnmc@gmail.com>
2023-12-08 12:55:48 -0500

    Use Image instead of img

--- app/warmup/page.tsx
+++ app/warmup/page.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
+
 export default function Warmup() {
   return (
     <main>
       <h1 className='text-7xl mx-auto text-center mt-48 font-mono'>Under Construction</h1>
-      <img className='mx-auto my-24' alt='construction blockade' width={200} height={200} src='https://cdn3.iconfinder.com/data/icons/travel-and-places-1-1/32/43-512.png'></img>
+      <Image className='mx-auto my-24' alt='construction blockade' width={200} height={200} src='https://cdn3.iconfinder.com/data/icons/travel-and-places-1-1/32/43-512.png'></Image>
     </main>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn3.iconfinder.com',
+        port: '',
+        pathname: '/data/icons/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
